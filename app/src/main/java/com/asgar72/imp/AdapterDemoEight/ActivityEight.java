202beaca32d8504dee53b956a89a976573371ca2
Asgar <asgarabbas532@gmail.com>
2023-01-30 10:23:39 +0530

    Search_View using CustomAdaptor

--- app/src/main/java/com/asgar72/imp/AdapterDemoEight/ActivityEight.java
+++ app/src/main/java/com/asgar72/imp/AdapterDemoEight/ActivityEight.java
@@ -15,9 +15,8 @@ public class ActivityEight extends AppCompatActivity {
 
     ListView listview_searchone;
     SearchView searchView_one;
+    ArrayList names;
 
-
-    String[] names={"Asgar","Hasan","Shajar","Aman","Taqi","Naqi","Shabab","Meezan","Fahmi","Shuja","Akbar"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +25,47 @@ public class ActivityEight extends AppCompatActivity {
         listview_searchone = findViewById(R.id.listview_searchone);
         searchView_one = findViewById(R.id.searchView_one);
 
+        names = new ArrayList();
+        names.add("Asgar");
+        names.add("Shajar");
+        names.add("Shabab");
+        names.add("Ali");
+        names.add("Aman");
+        names.add("Shuja");
+        names.add("Arkan");
+        names.add("Akbar");
+        names.add("Rehan");
+        names.add("Atahar");
 
+        SearchAdaptor searchAdaptor = new SearchAdaptor(this, names);
+        listview_searchone.setAdapter(searchAdaptor);
 
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,names);
-        listview_searchone.setAdapter(arrayAdapter);
+        searchView_one.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String s) {
+                searchAdaptor.myFilter(s);
+                return false;
+            }
+        });
+
+        //This is for normal search view
+        //      ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line,names);
+        //       listview_searchone.setAdapter(adapter);
+//        searchView_one.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String query) { //jaise hee search btn ke click hoga toh ye method execute hoga.
+//                return false;
+//            }
+//            @Override
+//            public boolean onQueryTextChange(String newText) { //jaise hee text type hoga oo search krne lagega
+//                adapter.getFilter().filter(newText);
+//                return false;
+//            }
+//        });
     }
 }
---
+++ app/src/main/java/com/asgar72/imp/AdapterDemoEight/SearchAdaptor.java
@@ -0,0 +1,69 @@
+package com.asgar72.imp.AdapterDemoEight;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.asgar72.imp.R;
+
+import java.util.ArrayList;
+
+public class SearchAdaptor extends BaseAdapter {
+
+    Context context;
+    ArrayList al_names;
+    LayoutInflater layoutInflater;
+
+    ArrayList al_new_names;
+
+    //this is constructor
+    public SearchAdaptor(Context context, ArrayList names) {
+        this.context = context;
+        this.al_names = names;
+        layoutInflater = LayoutInflater.from(context);
+
+        //sare name ko aik alag array mai store krna
+        al_new_names = new ArrayList();
+        al_new_names.addAll(al_names);
+    }
+
+    @Override
+    public int getCount() {
+        return al_names.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View view1 = layoutInflater.inflate(R.layout.ui_view_eight, null);
+        TextView tv = view1.findViewById(R.id.textview_eight);
+        tv.setText(al_names.get(i).toString());
+        return view1;
+    }
+
+    // this is my method
+    public void myFilter(String str)
+    {
+        al_names.clear();
+
+        for (Object name_obj : al_new_names) {
+            if (name_obj.toString().contains(str)) // ye check krega ke maine jo search bar mai type kiya hai oo uske andar hai ya nahi
+            {
+                al_names.add(name_obj.toString());
+            }
+        }
+        notifyDataSetChanged(); // ye wala method dubara se getView method ko call krega our phir se list create krega
+    }
+}
